91b3933f30d11ada2d238f69782c3955db358074
Riccardo <rtrocca@gmail.com>
2025-09-21 22:24:11 +0200

    ideas

--- scratchpad.js
+++ scratchpad.js
@@ -8,6 +8,7 @@ let states = [
     }
 ]
 
+// Allow to call on("message", handler) multiple times, so that the handle can change dynamically.
 on("message", (evt) => {
     llm.process(evt.data.text, history)
     llm.on("response", (response) => {
@@ -20,3 +21,23 @@ on("tool", (evt) => {
     write("With arguments: " + JSON.stringify(evt.data.args));
     return { result: "Tool execution result" };
 });
+
+
+// Example of registering a tool
+// TODO: what's the scope of this registration? In SCXML I can have a "local" registration
+// unregister?
+// make it part of the session?
+
+register_tool({
+    name: "getCurrentTime",
+    description: "Get the current system time",
+    parameters: {},
+    execute: () => {
+        return new Date().toString();
+    }
+})
+
+// Mimic the sessionStorage API, at least partially
+sessionStorage.clear()
+sessionStorage.setItem("state", "main")
+sessionStorage.getItem("state")
